54077f461110014ff2a738ad0ec69803ecf9f506
Griffin Kowash <gkowash@gmail.com>
2025-02-07 18:41:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Rational Method Output Parser - v1.0.0
+Created by Griffin Kowash, 7 February 2025
+
+To report bugs or request improvements, please open a Github issue or email gkowash@gmail.com.
+
+## Summary
+This script parses output files from a rational method hydrology model, extracts relevant
+flow data, and writes the processed information to CSV format. Additionally, if the 
+"tabulate" package is available, it can print the extracted data to the command line in a formatted table.
+
+## Functionality
+- Reads a text-based hydrology model output file.
+- Identifies and extracts flow rate and time of concentration (TOC) data.
+- Supports different rational method command types, mapped via the `CommandCase` enum.
+- Outputs extracted data as a CSV file.
+- Optionally prints the extracted data in a tabular format.
+
+## Dependencies
+- Requires Python >=3.12
+- Optional: The `tabulate` package for pretty-printing tables.
+
+## Setup
+Download or clone the repository at [insert repo URL] and move it to the desired location on your machine. Extract the zip file, if necessary.
+
+Ensure that Python 3.12 or greater is installed on the machine. To check the current python installation, run:
+
+```bat
+python --version
+```
+
+Install the `tabulate` package using your preferred package manager. For most users, the following command is sufficient:
+
+```bat
+pip install tabulate
+```
+
+Finally, add the location of RMParse to the "Path" environment variable. This can be done as follows:
+1. Open the Environment Variables menu, which can be acccessed from the Windows search bar.
+2. Click the Environment Variables button at the bottom of the menu.
+3. Select the Path field under "User variables" and click Edit.
+4. Copy the path to the RMParse repository (on Windows 11, right-click and select "Copy as path").
+5. In the Environment Variable editor, click New, paste in the copied path, and click OK.
+
+Please contact Griffin if support for Linux is needed.
+
+## Usage
+Run the script from the command line with:
+
+```bat
+rmparse <file1> <file2> ... [-d DIGITS] [-p]
+```
+
+Arguments:
+- `files`: One or more rational method output files to parse.
+- `-d`, `--digits`: Number of decimal places for numeric output (default: 2).
+- `-p`, `--print`: Print the extracted data in a table format if the "tabulate" package is available.
+
+<!-- A set of input files for testing purposes is provided at `RMParse/test_files`. -->
+
+If a non-default version of Python must be specified, the `rmparse.py` script can be run directly:
+
+```bat
+<path/to/python.exe> rmparse.py <file1> <file2> ... [-d DIGITS] [-p]
+```
+
+## Example output
+
+```text
+Wrote to csv file at .\test_files\testA.csv
+
+| Nodes   |   Q (CFS) |   TC (min) |
+|---------|-----------|------------|
+| 101-102 |      4.26 |      17.00 |
+| 102-102 |      6.65 |      17.00 |
+| 102-103 |     14.69 |      22.19 |
+| 103-104 |     17.94 |      23.70 |
+```
---
+++ rmparse.py
@@ -0,0 +1,247 @@
+"""
+Rational Method Output Parser
+
+This script parses output files from a rational method hydrology model, extracts relevant
+flow data, and writes the processed information to CSV format. Additionally, if the 
+"tabulate" package is available, it can print the extracted data in a formatted table.
+
+Run the script from the command line with:
+    python rmparse.py <file1> <file2> ... [-d DIGITS] [-p]
+
+To use the more concise "rmparse" command, add the RMParse repository to the Path environment
+variable.
+"""
+
+import os
+import csv
+import argparse
+import warnings
+from enum import Enum, auto
+from typing import Tuple, List
+
+try:
+    from tabulate import tabulate
+    PRINT_DATA = True
+except Exception as e:
+    PRINT_DATA = False
+    print('Unable to print data; failed to import package "tabulate".')
+
+NEW_SECTION_TEXT = 'Process from Point/Station'
+CONFLUENCE_SUMMARY_TEXT = 'Summary of stream data'
+
+class InsufficientDataError(Exception):
+    """Throw exception when flow rate and TOC are not determined before the next header line is encountered."""
+    def __init__(self, message: str, code: int = 0):
+        super().__init__(message)
+        self.code = code
+
+class CommandCase(Enum):
+    """Cases for rational method commands."""
+    INITIAL_AREA = auto()
+    STREET_FLOW = auto()
+    STREET_INLET = auto()
+    SUBAREA_ADDITION = auto()
+    PIPEFLOW_PROGRAM = auto()
+    PIPEFLOW_USER = auto()
+    CHANNEL_IMPROVED = auto()
+    CHANNEL_IRREGULAR = auto()
+    USER = auto()
+    CONFLUENCE_MINOR = auto()
+    CONFLUENCE_MAIN = auto()
+
+class ParserState(Enum):
+    """Cases for parser state."""
+    SEARCHING = auto()
+    PARSING_COMMAND = auto()
+    PARSING_STATS = auto()
+    STORING_DATA = auto()
+
+COMMAND_MAP = {
+    CommandCase.INITIAL_AREA: "**** INITIAL AREA EVALUATION ****",
+    CommandCase.STREET_FLOW: "**** STREET FLOW TRAVEL TIME",
+    CommandCase.STREET_INLET: "(command flag undefined)", #flag unavailable at time of writing; set if required
+    CommandCase.SUBAREA_ADDITION: "**** SUBAREA FLOW ADDITION ****",
+    CommandCase.PIPEFLOW_PROGRAM: "**** PIPEFLOW TRAVEL TIME (Program estimated size) ****",
+    CommandCase.PIPEFLOW_USER: "**** PIPEFLOW TRAVEL TIME (User specified size) ****",
+    CommandCase.CHANNEL_IMPROVED: "**** IMPROVED CHANNEL TRAVEL TIME ****",
+    CommandCase.CHANNEL_IRREGULAR: "**** IRREGULAR CHANNEL FLOW TRAVEL TIME ****",
+    CommandCase.USER: "**** USER DEFINED FLOW INFORMATION AT A POINT ****",
+    CommandCase.CONFLUENCE_MINOR: "**** CONFLUENCE OF MINOR STREAMS ****",
+    CommandCase.CONFLUENCE_MAIN: "**** CONFLUENCE OF MAIN STREAMS ****"
+}
+
+FLOWRATE_MAP = {
+    CommandCase.INITIAL_AREA: "Subarea runoff",
+    CommandCase.STREET_FLOW: "Total runoff",
+    CommandCase.STREET_INLET: "(command flag undefined)", #flag unavailable at time of writing; set if required
+    CommandCase.SUBAREA_ADDITION: 'Total runoff',
+    CommandCase.PIPEFLOW_PROGRAM: 'Required pipe flow',
+    CommandCase.PIPEFLOW_USER: 'Required pipe flow',
+    CommandCase.CHANNEL_IMPROVED: 'Total runoff',
+    CommandCase.CHANNEL_IRREGULAR: 'Total runoff',
+    CommandCase.USER: 'Total runoff',
+    CommandCase.CONFLUENCE_MINOR: 'Total flow rate',
+    CommandCase.CONFLUENCE_MAIN: 'Total flow rate'
+}
+
+TOC_MAP = {
+    CommandCase.INITIAL_AREA: "Initial area time of concentration",
+    CommandCase.STREET_FLOW: "TC =",
+    CommandCase.STREET_INLET: "(command flag undefined)", #flag unavailable at time of writing; set if required
+    CommandCase.SUBAREA_ADDITION: 'Time of concentration',
+    CommandCase.PIPEFLOW_PROGRAM: 'Time of concentration (TC)',
+    CommandCase.PIPEFLOW_USER: 'Time of concentration (TC)',
+    CommandCase.CHANNEL_IMPROVED: 'Time of concentration',
+    CommandCase.CHANNEL_IRREGULAR: 'Time of concentration',
+    CommandCase.USER: 'TC =',
+    CommandCase.CONFLUENCE_MINOR: 'Time of concentration',
+    CommandCase.CONFLUENCE_MAIN: 'Time of concentration'
+}
+
+def main() -> None:
+    """Parse rational method output file, write data to csv, and print to console."""
+    filepaths, precision, print_data = parse_args()
+    for filepath in filepaths:
+        lines = read_file(filepath)
+        data = parse_data_from(lines)
+        csv_filepath = get_csv_filepath(filepath)
+        write_to_csv(data, precision=precision, filepath=csv_filepath)
+        if print_data and PRINT_DATA:
+            print_to_console(data, precision=precision)
+
+def parse_args() -> Tuple[List[str], int, bool]:
+    """Parse command line options."""
+    parser = argparse.ArgumentParser(
+        prog='Rational method parser',
+        description='Parses rational method output files and converts data to csv format.')
+    parser.add_argument('files', nargs='+')
+    parser.add_argument('-d', '--digits', type=int, default=2)
+    parser.add_argument('-p', '--print', action='store_true')
+    args = parser.parse_args()
+   
+    # Warning for csv files
+    if any(file.endswith('.csv') for file in args.files):
+        warnings.warn('CSV file provided. Unexpected behavior will occur if attempting to parse the output of a previous RMParse execution.')
+
+    return args.files, args.digits, args.print
+
+def read_file(filepath: str) -> List[str]:
+    """Read text file into list of lines."""
+    with open(filepath, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    return lines
+
+def parse_data_from(lines: List[str]) -> List[Tuple[str, float, float]]:
+    """Extract nodes, time of concentration, and flow rate from list of file lines."""
+    state = ParserState.SEARCHING
+    found_confluence_summary = False
+    command = None
+    nodes = None
+    flowrate = None
+    toc = None
+    data = []
+
+    for i, line in enumerate(lines):
+        if CONFLUENCE_SUMMARY_TEXT in line:
+            found_confluence_summary = True
+
+        if NEW_SECTION_TEXT in line:
+            if state == ParserState.SEARCHING:
+                node1, node2 = get_nodes(line)
+                state = ParserState.PARSING_COMMAND
+            # If a confluence entry is missing "Summary of stream data", we don't expect to find flow data
+            elif command in (CommandCase.CONFLUENCE_MAIN, CommandCase.CONFLUENCE_MINOR) and not found_confluence_summary:
+                state = ParserState.SEARCHING
+                continue
+            else:
+                raise InsufficientDataError(f'Failed to determine flow rate and time of concentration before next command header.\
+                    \n\tFinal line: {i}\
+                    \n\tCommand: {command}\
+                    \n\tTOC: {toc}\
+                    \n\tFlow: {flowrate}')
+            
+        elif state == ParserState.PARSING_COMMAND:
+            command = get_command_case(line)
+            if command is not None:
+                nodes = format_nodes(node1, node2, command)
+                state = ParserState.PARSING_STATS
+            
+        elif state == ParserState.PARSING_STATS:
+            if FLOWRATE_MAP[command] in line:
+                flowrate = get_flowrate(line)
+            elif TOC_MAP[command] in line:
+                toc = get_toc(line)
+            if flowrate is not None and toc is not None:
+                state = ParserState.STORING_DATA
+        
+        if state == ParserState.STORING_DATA:
+            data.append((nodes, flowrate, toc))
+            toc = None
+            flowrate = None
+            found_confluence_summary = False
+            state = ParserState.SEARCHING
+
+    return data
+
+def get_nodes(text: str) -> str:
+    """Parse text and return formatted note string."""
+    node1, node2 = parse_nodes(text)
+    return node1, node2
+
+def parse_nodes(text: str) -> Tuple[int, int]:
+    """Read node numbers and return formatted string."""
+    text_split = text.split()
+    node1 = int(float(text_split[3]))
+    node2 = int(float(text_split[6]))
+    return node1, node2
+    
+def format_nodes(node1: int, node2: int, command: CommandCase) -> str:
+    """Format a pair of nodes into a string."""
+    if command in (CommandCase.CONFLUENCE_MAIN, CommandCase.CONFLUENCE_MINOR):
+        return f'*{node1}-{node2}'
+    else:
+        return f'{node1}-{node2}'
+
+def get_command_case(text: str) -> ParserState | None:
+    """Parse command type, returning None if unspecified in text."""
+    for command_case, flag in COMMAND_MAP.items():
+        if flag in text:
+            return command_case
+    return None
+
+def get_flowrate(text: str) -> float:
+    """Get flowrate from text."""
+    flowrate_str = text.strip().split()[-1]
+    flowrate = float(flowrate_str.split('(')[0]) #remove (CFS) suffix
+    return flowrate
+
+def get_toc(text: str) -> float:
+    """Get time of concentration from text."""
+    return float(text.strip().split()[-2])
+
+def get_csv_filepath(filepath: str) -> str:
+    """Generate csv filepath with same name and location as source."""
+    basename, _ = os.path.splitext(filepath)
+    return basename + '.csv'
+
+def print_to_console(data: List[Tuple[str, float, float]], precision: int = 2) -> None:
+    """Print data to command line in a pretty table."""
+    headers = ['Nodes', 'Q (CFS)', 'TC (min)'] #TODO: move outside of print_data and write_to_csv
+    floatfmt = f'.{precision}F'
+    tablefmt = 'github' #see "tabulate" docs for more formatting options
+    print()
+    print(tabulate(data, headers=headers, tablefmt=tablefmt, floatfmt=floatfmt))
+    print()
+
+def write_to_csv(data: List[Tuple[str, float, float]], filepath: str, precision: int) -> None:
+    """Output rational method data to .csv file."""
+    headers = ['Nodes', 'Q', 'TC']  #TODO: add storm year as suffix to Q
+    with open(filepath, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+        for node_str, toc, flowrate in data:
+            writer.writerow([node_str, f'{toc:.{precision}F}', f'{flowrate:.{precision}F}'])
+    print(f'Wrote to csv file at {filepath}')
+
+if __name__ == '__main__':
+    main()
