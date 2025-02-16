7f5a5cce565772eea71e0e4c33b60ed247bffcb5
Griffin Kowash <gkowash@gmail.com>
2025-02-15 22:10:25 -0700

    add county template files

--- rmparse.py
+++ rmparse.py
@@ -12,12 +12,16 @@ To use the more concise "rmparse" command, add the RMParse repository to the Pat
 variable.
 """
 
+import sys
 import os
 import csv
 import argparse
 import warnings
+from pathlib import Path
 from enum import Enum, auto
 from typing import Tuple, List
+
+import yaml
 
 try:
     from tabulate import tabulate
@@ -26,28 +30,11 @@ except Exception as e:
     PRINT_DATA = False
     print('Unable to print data; failed to import package "tabulate".')
 
-NEW_SECTION_TEXT = 'Process from Point/Station'
-CONFLUENCE_SUMMARY_TEXT = 'Summary of stream data'
-
 class InsufficientDataError(Exception):
     """Throw exception when flow rate and TOC are not determined before the next header line is encountered."""
     def __init__(self, message: str, code: int = 0):
         super().__init__(message)
         self.code = code
-
-class CommandCase(Enum):
-    """Cases for rational method commands."""
-    INITIAL_AREA = auto()
-    STREET_FLOW = auto()
-    STREET_INLET = auto()
-    SUBAREA_ADDITION = auto()
-    PIPEFLOW_PROGRAM = auto()
-    PIPEFLOW_USER = auto()
-    CHANNEL_IMPROVED = auto()
-    CHANNEL_IRREGULAR = auto()
-    USER = auto()
-    CONFLUENCE_MINOR = auto()
-    CONFLUENCE_MAIN = auto()
 
 class ParserState(Enum):
     """Cases for parser state."""
@@ -56,54 +43,84 @@ class ParserState(Enum):
     PARSING_STATS = auto()
     STORING_DATA = auto()
 
-COMMAND_MAP = {
-    CommandCase.INITIAL_AREA: "**** INITIAL AREA EVALUATION ****",
-    CommandCase.STREET_FLOW: "**** STREET FLOW TRAVEL TIME",
-    CommandCase.STREET_INLET: "(command flag undefined)", #flag unavailable at time of writing; set if required
-    CommandCase.SUBAREA_ADDITION: "**** SUBAREA FLOW ADDITION ****",
-    CommandCase.PIPEFLOW_PROGRAM: "**** PIPEFLOW TRAVEL TIME (Program estimated size) ****",
-    CommandCase.PIPEFLOW_USER: "**** PIPEFLOW TRAVEL TIME (User specified size) ****",
-    CommandCase.CHANNEL_IMPROVED: "**** IMPROVED CHANNEL TRAVEL TIME ****",
-    CommandCase.CHANNEL_IRREGULAR: "**** IRREGULAR CHANNEL FLOW TRAVEL TIME ****",
-    CommandCase.USER: "**** USER DEFINED FLOW INFORMATION AT A POINT ****",
-    CommandCase.CONFLUENCE_MINOR: "**** CONFLUENCE OF MINOR STREAMS ****",
-    CommandCase.CONFLUENCE_MAIN: "**** CONFLUENCE OF MAIN STREAMS ****"
-}
+class CommandCase(Enum):
+    """Cases for rational method commands."""
+    INITIAL_AREA = "INITIAL_AREA"
+    STREET_FLOW = "STREET_FLOW"
+    STREET_INLET = "STREET_INLET"
+    SUBAREA_ADDITION = "SUBAREA_ADDITION"
+    PIPEFLOW_PROGRAM = "PIPEFLOW_PROGRAM"
+    PIPEFLOW_USER = "PIPEFLOW_USER"
+    CHANNEL_IMPROVED = "CHANNEL_IMPROVED"
+    CHANNEL_IRREGULAR = "CHANNEL_IRREGULAR"
+    USER = "USER"
+    CONFLUENCE_MINOR = "CONFLUENCE_MINOR"
+    CONFLUENCE_MAIN = "CONFLUENCE_MAIN"
 
-FLOWRATE_MAP = {
-    CommandCase.INITIAL_AREA: "Subarea runoff",
-    CommandCase.STREET_FLOW: "Total runoff",
-    CommandCase.STREET_INLET: "(command flag undefined)", #flag unavailable at time of writing; set if required
-    CommandCase.SUBAREA_ADDITION: 'Total runoff',
-    CommandCase.PIPEFLOW_PROGRAM: 'Required pipe flow',
-    CommandCase.PIPEFLOW_USER: 'Required pipe flow',
-    CommandCase.CHANNEL_IMPROVED: 'Total runoff',
-    CommandCase.CHANNEL_IRREGULAR: 'Total runoff',
-    CommandCase.USER: 'Total runoff',
-    CommandCase.CONFLUENCE_MINOR: 'Total flow rate',
-    CommandCase.CONFLUENCE_MAIN: 'Total flow rate'
-}
+# class Counties(Enum):
+#     """Supported counties with YAML template files."""
+#     SAN_BERNARDINO = "San Bernardino"
+#     RIVERSIDE = "Riverside"
 
-TOC_MAP = {
-    CommandCase.INITIAL_AREA: "Initial area time of concentration",
-    CommandCase.STREET_FLOW: "TC =",
-    CommandCase.STREET_INLET: "(command flag undefined)", #flag unavailable at time of writing; set if required
-    CommandCase.SUBAREA_ADDITION: 'Time of concentration',
-    CommandCase.PIPEFLOW_PROGRAM: 'Time of concentration (TC)',
-    CommandCase.PIPEFLOW_USER: 'Time of concentration (TC)',
-    CommandCase.CHANNEL_IMPROVED: 'Time of concentration',
-    CommandCase.CHANNEL_IRREGULAR: 'Time of concentration',
-    CommandCase.USER: 'TC =',
-    CommandCase.CONFLUENCE_MINOR: 'Time of concentration',
-    CommandCase.CONFLUENCE_MAIN: 'Time of concentration'
-}
+COUNTIES = ('San Bernardino', 'Riverside')
+TEMPLATE_DIR = 'templates'
+# COMMAND_MAP = {
+#     CommandCase.INITIAL_AREA: "INITIAL AREA EVALUATION",
+#     CommandCase.STREET_FLOW: "STREET FLOW TRAVEL TIME",
+#     CommandCase.STREET_INLET: "(command flag undefined)",
+#     CommandCase.SUBAREA_ADDITION: "SUBAREA FLOW ADDITION",
+#     CommandCase.PIPEFLOW_PROGRAM: "PIPEFLOW TRAVEL TIME (Program estimated size)",
+#     CommandCase.PIPEFLOW_USER: "PIPEFLOW TRAVEL TIME (User specified size)",
+#     CommandCase.CHANNEL_IMPROVED: "IMPROVED CHANNEL TRAVEL TIME",
+#     CommandCase.CHANNEL_IRREGULAR: "IRREGULAR CHANNEL FLOW TRAVEL TIME",
+#     CommandCase.USER: "USER DEFINED FLOW INFORMATION AT A POINT",
+#     CommandCase.CONFLUENCE_MINOR: "CONFLUENCE OF MINOR STREAMS",
+#     CommandCase.CONFLUENCE_MAIN: "CONFLUENCE OF MAIN STREAMS"
+# }
+
+# FLOWRATE_MAP = {
+#     CommandCase.INITIAL_AREA: "Subarea runoff",
+#     CommandCase.STREET_FLOW: "Total runoff",
+#     CommandCase.STREET_INLET: "(command flag undefined)",
+#     CommandCase.SUBAREA_ADDITION: 'Total runoff',
+#     CommandCase.PIPEFLOW_PROGRAM: 'Required pipe flow',
+#     CommandCase.PIPEFLOW_USER: 'Required pipe flow',
+#     CommandCase.CHANNEL_IMPROVED: 'Total runoff',
+#     CommandCase.CHANNEL_IRREGULAR: 'Total runoff',
+#     CommandCase.USER: 'Total runoff',
+#     CommandCase.CONFLUENCE_MINOR: 'Total flow rate',
+#     CommandCase.CONFLUENCE_MAIN: 'Total flow rate'
+# }
+
+# TOC_MAP = {
+#     CommandCase.INITIAL_AREA: "Initial area time of concentration =",
+#     CommandCase.STREET_FLOW: "TC =",
+#     CommandCase.STREET_INLET: "(command flag undefined)",
+#     CommandCase.SUBAREA_ADDITION: 'Time of concentration =',
+#     CommandCase.PIPEFLOW_PROGRAM: 'Time of concentration (TC) =',
+#     CommandCase.PIPEFLOW_USER: 'Time of concentration (TC) =',
+#     CommandCase.CHANNEL_IMPROVED: 'Time of concentration =',
+#     CommandCase.CHANNEL_IRREGULAR: 'Time of concentration =',
+#     CommandCase.USER: 'TC =',
+#     CommandCase.CONFLUENCE_MINOR: 'Time of concentration =',
+#     CommandCase.CONFLUENCE_MAIN: 'Time of concentration ='
+# }
+
+# NEW_SECTION_TEXT = 'Process from Point/Station'
+# CONFLUENCE_SUMMARY_TEXT = 'Summary of stream data'
+
+COMMAND_MAP = None
+FLOWRATE_MAP = None
+TOC_MAP = None
+NEW_SECTION_TEXT = None
+CONFLUENCE_SUMMARY_TEXT = None
 
 def main() -> None:
     """Parse rational method output file, write data to csv, and print to console."""
     filepaths, precision, print_data = parse_args()
     for filepath in filepaths:
         lines = read_file(filepath)
-        data = parse_data_from(lines)
+        data = parse_data_from_lines(lines)
         csv_filepath = get_csv_filepath(filepath)
         write_to_csv(data, precision=precision, filepath=csv_filepath)
         if print_data and PRINT_DATA:
@@ -129,11 +146,32 @@ def read_file(filepath: str) -> List[str]:
     """Read text file into list of lines."""
     with open(filepath, 'r', encoding='utf-8') as file:
         lines = file.readlines()
+    lines = [line.lower() for line in lines]
     return lines
 
-def parse_data_from(lines: List[str]) -> List[Tuple[str, float, float]]:
+def load_template_file(filepath: str) -> None:
+    """Populate county-specific text mappings from YAML file."""
+    global COMMAND_MAP, FLOWRATE_MAP, TOC_MAP, NEW_SECTION_TEXT, CONFLUENCE_SUMMARY_TEXT
+    with open(filepath, 'r') as file:
+        data = yaml.safe_load(file)
+    COMMAND_MAP = {CommandCase[key]: value.lower() for key, value in data['commands'].items()}
+    FLOWRATE_MAP = {CommandCase[key]: value.lower() + ' =' for key, value in data['flowrate'].items()}
+    TOC_MAP = {CommandCase[key]: value.lower() + ' =' for key, value in data['time-of-concentration'].items()}
+    NEW_SECTION_TEXT = data['new-section-text'].lower()
+    CONFLUENCE_SUMMARY_TEXT = data['confluence-summary-text'].lower()
+
+def load_template_for_county(county: str) -> None:
+    """Load template file corresponding to county name."""
+    filename = county + '.yaml'
+    filepath = Path(__file__).parent / TEMPLATE_DIR / filename
+    if not filepath.exists():
+        raise FileNotFoundError(f'Could not find county template at {filepath}')
+    load_template_file(filepath)
+
+def parse_data_from_lines(lines: List[str]) -> List[Tuple[str, float, float]]:
     """Extract nodes, time of concentration, and flow rate from list of file lines."""
     state = ParserState.SEARCHING
+    county = None
     found_confluence_summary = False
     command = None
     nodes = None
@@ -142,6 +180,12 @@ def parse_data_from(lines: List[str]) -> List[Tuple[str, float, float]]:
     data = []
 
     for i, line in enumerate(lines):
+        if county is None:
+            county = next((county for county in COUNTIES if county.lower() in line), None)
+            if county is None:
+                continue
+            load_template_for_county(county)
+
         if CONFLUENCE_SUMMARY_TEXT in line:
             found_confluence_summary = True
 
@@ -180,6 +224,10 @@ def parse_data_from(lines: List[str]) -> List[Tuple[str, float, float]]:
             flowrate = None
             found_confluence_summary = False
             state = ParserState.SEARCHING
+
+    if county is None:
+        print('Failed to identify county from output file. Ensure that the county is specified in the COUNTIES variable and has a corresponding YAML template.')
+        sys.exit(1)
 
     return data
 
