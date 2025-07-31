aa1f49687fd268257edcd6fa9ff59105a66e128f
Griffin Kowash <gkowash@gmail.com>
2025-07-31 09:29:59 -0600

    add requirements.txt + modify installer to accept --deps [pip/conda] to optionally install packages

--- install.bat
+++ install.bat
@@ -1,5 +1,19 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+
+:: Parse arguments
+set DEPENDENCY_INSTALL_METHOD=
+:parse_args
+if "%~1"=="" goto done_parse
+if "%~1"=="--deps" (
+    shift
+    set DEPENDENCY_INSTALL_METHOD=%~2
+)
+shift
+goto parse_args
+:done_parse
+
+echo Dependency install method: %DEPENDENCY_INSTALL_METHOD%
 
 :: Check if any version of Python is installed
 python --version >nul 2>&1
@@ -30,29 +44,30 @@ if %MAJOR% EQU 3 if %MINOR% LSS 12 (
 )
 echo Python 3.12 or greater is installed.
 
-:: Install required Python packages
-echo Installing required packages...
-pip install tabulate PyYAML
-
-:: Check for errors
+:: Install required Python packages, if specified
+if /i "%DEPENDENCY_INSTALL_METHOD%"=="conda" (
+    echo Installing required Python packages using conda...
+    conda install --file requirements.txt -y
+) else if /i "%DEPENDENCY_INSTALL_METHOD%"=="pip" (
+    echo Installing required Python packages using pip...
+    pip install -r requirements.txt
+) else (
+    echo Python dependencies not installed. Install requirements.txt manually or rerun this script with the "--deps [pip/conda]" flag.
+)
 if %errorlevel% neq 0 (
     echo Error: Failed to install required packages.
-    echo Ensure Python and pip are installed and try again.
+    echo Ensure Python and pip or conda are installed and try again.
     exit /b 1
 )
 
 :: Add RMParse to the user PATH environment variable
 echo Adding RMParse to the user PATH...
 
-:: Get the current user PATH
 for /f "tokens=2* delims= " %%A in ('reg query "HKCU\Environment" /v Path 2^>nul') do set "USER_PATH=%%B"
 
-:: Get the directory of this script (assumed to be the top level of RMParse)
 set "RMPARSE_PATH=%~dp0"
-:: Remove trailing backslash if present
 if "%RMPARSE_PATH:~-1%"=="\" set "RMPARSE_PATH=%RMPARSE_PATH:~0,-1%"
 
-:: Add the path to user environment variable if not present
 echo %USER_PATH% | findstr /i /c:"%RMPARSE_PATH%" >nul
 if %errorlevel% neq 0 (
     setx PATH "%USER_PATH%;%RMPARSE_PATH%"
